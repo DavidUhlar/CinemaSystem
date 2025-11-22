1e72a6651b0c8f09df1fe39e1f55c900c65989ac
DavidUhlar <dadododo02@gmail.com>
2025-11-22 21:44:21 +0100

    builder for reservation

---
+++ Services/DesignPatterns/Builder/IReservationBuilder.cs
@@ -0,0 +1,13 @@
+﻿using CinemaSystem.Models;
+
+namespace CinemaSystem.Services.DesignPatterns.Builder
+{
+    public interface IReservationBuilder
+    {
+        IReservationBuilder Reset();
+        IReservationBuilder SetTicket(Ticket ticket);
+        IReservationBuilder SetTickets(List<Ticket> ticket);
+        IReservationBuilder SetCustomer(Customer customer);
+        Reservation Build();
+    }
+}
---
+++ Services/DesignPatterns/Builder/ReservationDirector.cs
@@ -0,0 +1,40 @@
+﻿using CinemaSystem.Models;
+
+namespace CinemaSystem.Services.DesignPatterns.Builder
+{
+    public class ReservationDirector
+    {
+        public Reservation CreateStandardReservation(Customer customer, List<Ticket> tickets)
+        {
+            var builder = new ReservationBuilder();
+
+            return builder
+                .SetCustomer(customer)
+                .SetTickets(tickets)
+                .Build();
+        }
+        
+        public Reservation CreateGroupReservation(Customer customer, List<Ticket> tickets)
+        {
+            var builder = new GroupReservationBuilder();
+
+            return builder
+                .SetCustomer(customer)
+                .SetTickets(tickets)
+                .Build();
+        }
+        
+
+        public Reservation CreateReservation(Customer customer, List<Ticket> tickets)
+        {
+            if (tickets.Count >= 5)
+            {
+                return CreateGroupReservation(customer, tickets);
+            }
+            else
+            {
+                return CreateStandardReservation(customer, tickets);
+            }
+        }
+    }
+}
