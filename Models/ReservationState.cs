fd7d85897eb15c857cb0f85caab58bf16db3118f
DavidUhlar <dadododo02@gmail.com>
2025-11-28 19:57:25 +0100

    reservation facade

---
+++ Models/ReservationState.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaSystem.Models
+{
+    public class ReservationState
+    {
+        public int EventId { get; set; }
+        public int CustomerId { get; set; }
+        public List<Ticket> Tickets { get; set; } = new List<Ticket>();
+    }
+}
